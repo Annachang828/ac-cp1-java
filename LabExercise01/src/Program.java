172b9b2175fce27230988a589fe7c33ba56bd1d0
TSAICHUN <chan0558@algonquinlive.com>
2023-06-26 15:18:45 -0400

    Lab01 exercise

---
+++ LabExercise01/src/Program.java
@@ -0,0 +1,26 @@
+/*
+ * Student Name: TSAI CHUN CHANG
+ * Student Number: 041095591
+ * Lab Professor: Professor David Houtman
+ * Due Date: May 26, 2023.
+ * Modified: May 16, 2023.
+ * Course Number: CST8116
+ * Lab Section Number: 341
+ * Description: Lab Exercise 01
+ */
+
+/* Description: this class outputs a greeting and student's name from within method main. */
+
+public class Program {
+
+	public static void main(String[] args) {
+
+			String name = "TSAI CHUN CHANG";
+			System.out.println("Welcome to CST8116!");
+			System.out.print("Java says: Hello ");
+			System.out.print(name);
+			System.out.println();// new-line
+			System.out.println("Program modified by " + name);
+	}
+
+}
